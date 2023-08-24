f7adae9125b524266f7e2bb3c61399663d583713
Julio Matias <juliocesark2@hotmail.com>
2023-08-24 02:02:02 -0300

    requesito 1 ok

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-
+const getSpeciesByIds = (...ids) => {
+  if (ids.length === 0) return [];
+  if (ids.length === 1) return data.species.filter((specie) => specie.id === ids[0]);
+  return ids.map((id) => data.species.find((specie) => specie.id === id));
 };
 
+// console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
