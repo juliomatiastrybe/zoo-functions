42c185d65c38abd28de90de043fa60edbbbd4ef4
Julio Matias <juliocesark2@hotmail.com>
2023-08-24 18:32:39 -0300

    requesito 1 reestruturando o if para atender melhor a condição imposta..

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,7 +2,7 @@ const data = require('../data/zoo_data');
 
 const getSpeciesByIds = (...ids) => {
   const { species } = data;
-  if (ids.length === 0) return [];
+  if (ids.length === 0 || !ids) return [];
   return ids.map((id) => species.find((specie) => specie.id === id));
 };
 
