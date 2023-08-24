1f4231236ebb4bc4f3bdf15fc4c7b86d2a0940c6
Julio Matias <juliocesark2@hotmail.com>
2023-08-24 18:28:43 -0300

    requesito 1 reestruturando a função para ficar mais identado e retirando lógica desnecessarias.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,9 +1,9 @@
 const data = require('../data/zoo_data');
 
 const getSpeciesByIds = (...ids) => {
+  const { species } = data;
   if (ids.length === 0) return [];
-  if (ids.length === 1) return data.species.filter((specie) => specie.id === ids[0]);
-  return ids.map((id) => data.species.find((specie) => specie.id === id));
+  return ids.map((id) => species.find((specie) => specie.id === id));
 };
 
 // console.log(getSpeciesByIds());
