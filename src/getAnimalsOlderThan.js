839e45d4769074289bcd33143e42eceae2ceb846
Julio Matias <juliocesark2@hotmail.com>
2023-08-24 22:22:08 -0300

    requesito 2 ok

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const { species } = data;
+  const findAnimal = species.find((specie) => specie.name.includes(animal));
+  // console.log(findAnimal);
+  if (findAnimal === undefined) return false;
+  return findAnimal.residents.every((resident) => resident.age >= age);
 };
+
+// console.log(getAnimalsOlderThan('ottders', 7));
 
 module.exports = getAnimalsOlderThan;
