784f10a82e74cdc1c3deadb2db32bcfd765cbe78
Julio Matias <juliocesark2@hotmail.com>
2023-08-29 23:12:52 -0300

    requisito 4 ok

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -4,8 +4,6 @@ const { employees } = data;
 
 const getMenagersIds = [...new Set(employees.flatMap((employee) => employee.managers))];
 
-console.log(getMenagersIds);
-
 const createArrayMenagersForIds = getMenagersIds.map((id) => {
   const matchingMenager = employees.find((employee) => employee.id.includes(id));
   return {
@@ -17,13 +15,25 @@ const createArrayMenagersForIds = getMenagersIds.map((id) => {
 // dos gerentes dinamicamente, sem a necessidade de ver no data.
 
 const isManager = (id) => {
-  const comparateIdsMenagers = createArrayMenagersForIds.every((manager) => manager.id === id);
+  const comparateIdsMenagers = createArrayMenagersForIds.some((managerId) => managerId.id === id);
   return comparateIdsMenagers;
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  const verifyMenagerId = isManager(managerId);
+  if (verifyMenagerId === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    const managedPeople = employees
+      .filter(({ managers }) => managers.includes(managerId))
+      .reduce((managed, { firstName, lastName }) => {
+        managed.push(`${firstName} ${lastName}`);
+        return managed;
+      }, []);
+    return managedPeople;
+  }
 };
-// console.log(isManager('id'));
+// console.log(isManager('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
+// console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 module.exports = { isManager, getRelatedEmployees };
