c62d02d6162eca7341b97179bae8bcd3bea4b5f5
Julio Matias <juliocesark2@hotmail.com>
2023-08-29 21:20:02 -0300

    requisito 4 com erro no test da função isManager, pós existe mais gerentes no data que no test da função.

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
+const getMenagersIds = [...new Set(employees.flatMap((employee) => employee.managers))];
+
+console.log(getMenagersIds);
+
+const createArrayMenagersForIds = getMenagersIds.map((id) => {
+  const matchingMenager = employees.find((employee) => employee.id.includes(id));
+  return {
+    name: matchingMenager.firstName,
+    id: matchingMenager.id,
+  };
+});
+// o array criado no createArrayMenagersForId somente para melhor vizualização
+// dos gerentes dinamicamente, sem a necessidade de ver no data.
+
 const isManager = (id) => {
-  // seu código aqui
+  const comparateIdsMenagers = createArrayMenagersForIds.every((manager) => manager.id === id);
+  return comparateIdsMenagers;
 };
 
 const getRelatedEmployees = (managerId) => {
   // seu código aqui
 };
+// console.log(isManager('id'));
 
 module.exports = { isManager, getRelatedEmployees };
