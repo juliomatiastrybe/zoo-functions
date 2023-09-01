82a7cddedd811f81ed920e1ef4be5b3d69617249
Julio Matias <juliocesark2@hotmail.com>
2023-08-31 23:40:08 -0300

    requisito 9 ok

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,52 @@
 const data = require('../data/zoo_data');
 
-const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+const { species } = data;
+const { hours } = data;
+
+const arrWeek = Object.keys(hours);
+
+const daysAvailabilitySpecie = (nameSpecie) => species
+  .find((specie) => specie.name === nameSpecie).availability;
+
+const filterSpeciesDay = (day) => {
+  if (day === 'Monday') {
+    return 'The zoo will be closed!';
+  }
+  const arrSpecieByday = species
+    .filter((specie) => specie.availability.includes(day))
+    .map((specie) => specie.name);
+  return arrSpecieByday;
 };
+
+const timeWeekAvailable = (dayWeek) => {
+  let dayReceived = arrWeek;
+  if (arrWeek.includes(dayWeek)) dayReceived = [dayWeek];
+  const objectScheduleWeek = dayReceived.reduce((objectWeek, day) => {
+    const arrTimeByDay = Object.values(hours[day]);
+    const objectDay = {
+      ...objectWeek,
+      [day]: {
+        officeHour: `Open from ${arrTimeByDay[0]}am until ${arrTimeByDay[1]}pm`,
+        exhibition: filterSpeciesDay(day),
+      },
+    };
+    if (day === 'Monday') {
+      objectDay.Monday.officeHour = 'CLOSED';
+    }
+    return objectDay;
+  }, {});
+  return objectScheduleWeek;
+};
+// console.log(timeWeekAvailable('Monday'));
+
+const getSchedule = (scheduleTarget = timeWeekAvailable()) => {
+  const getNameAnimals = species.map((specie) => specie.name);
+  if (getNameAnimals.includes(scheduleTarget)) return daysAvailabilitySpecie(scheduleTarget);
+  if (!arrWeek.includes(scheduleTarget)) return timeWeekAvailable();
+  if (arrWeek.includes(scheduleTarget)) return timeWeekAvailable(scheduleTarget);
+  return scheduleTarget;
+};
+
+// console.log(getSchedule());
 
 module.exports = getSchedule;
