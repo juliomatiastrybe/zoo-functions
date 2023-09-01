9efdd96a0332bae48545cca0dddeb4ae3510a99e
Julio Matias <juliocesark2@hotmail.com>
2023-09-01 10:49:53 -0300

    requisito 11 ok

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,46 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const { species, employees } = data;
+
+const getSpecie = (arrIdSpecie, param) => arrIdSpecie.map((id) => {
+  const specieFound = species.find((specie) => specie.id === id);
+  return specieFound[param];
+});
+
+const createEmployeeInfo = (employee) => {
+  const { id, firstName, lastName, responsibleFor } = employee;
+  return {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: getSpecie(responsibleFor, 'name'),
+    locations: getSpecie(responsibleFor, 'location'),
+  };
 };
+
+const findEmployee = (object) => {
+  switch (Object.keys(object)[0]) {
+  case 'name':
+    return employees
+      .find(({ firstName, lastName }) => firstName === object.name || lastName === object.name);
+  case 'id':
+    return employees.find(({ id }) => id === object.id);
+  default:
+    return null;
+  }
+};
+
+const getEmployeesCoverage = (object = {}) => {
+  if (Object.keys(object).length > 0) {
+    const foundEmployee = findEmployee(object);
+    console.log(foundEmployee);
+    if (foundEmployee) {
+      return createEmployeeInfo(foundEmployee);
+    }
+    throw new Error('Informações inválidas');
+  }
+  return employees.map(createEmployeeInfo);
+};
+
+// console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 
 module.exports = getEmployeesCoverage;
